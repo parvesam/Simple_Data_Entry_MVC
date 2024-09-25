b72aa8c1d75bea029cb4437bed2c15a54239b1cf
parvesam <parvesam@sheridancollege.ca>
2024-09-24 21:20:24 -0400

    "project initialized"

---
+++ BorrowEquipmentApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BorrowEquipmentApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
